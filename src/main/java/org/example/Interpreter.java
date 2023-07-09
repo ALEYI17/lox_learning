b1ffbeb637e7b05b10ac52de32849dece7881519
ALEYI17 <aleelaleyi@gmail.com>
2023-07-09 11:36:16 -0500

    Se agrego que el operador de suma pueda concatenar un string con un numero

--- src/main/java/org/example/Interpreter.java
+++ src/main/java/org/example/Interpreter.java
@@ -70,7 +70,12 @@ public class Interpreter implements Expr.Visitor<Object>{
                 if(left instanceof String && right instanceof  String){
                     return (String) left + (String) right;
                 }
-                throw  new RuntimeError(expr.operator,"Operands must be two numbers or two strings");
+                if(left instanceof Double && right instanceof String ){
+                    return (String) stringify(left) + (String) right;
+                } else if (left instanceof String && right instanceof Double) {
+                    return (String) left + (String) stringify(right);
+                }
+                throw  new RuntimeError(expr.operator,"Operands must be Number or String");
             case SLASH:
                 checkNumberOperand(expr.operator,left,right);
                 return (double)left / (double) right;
